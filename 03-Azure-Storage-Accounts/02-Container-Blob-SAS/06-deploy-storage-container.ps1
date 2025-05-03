79ffc071626dde22b576a3b0920c28d54f6063a0
CrisN28 <Crislerobando10@gmail.com>
2025-05-02 23:51:25 -0600

    Improve storage setup

--- 03-Azure-Storage-Accounts/02-Container-Blob-SAS/06-deploy-storage-container.ps1
+++ 03-Azure-Storage-Accounts/02-Container-Blob-SAS/06-deploy-storage-container.ps1
@@ -1,4 +1,6 @@
+# Deploy an ARM template to create a private blob container in Azure (Images)
+
 New-AzResourceGroupDeployment `
-  -ResourceGroupName "RG-Labs-AZ104" `
-  -TemplateFile "04-storage-container-template.json" `
-  -TemplateParameterFile "05-storage-container-parameters.json"
+  -ResourceGroupName "RG-Labs-AZ104" `  # Name of the resource group where the container will be deployed
+  -TemplateFile "04-storage-container-template.json" `  # ARM template defining the storage container configuration
+  -TemplateParameterFile "05-storage-container-parameters.json"  # Parameter file specifying storage account and container name
