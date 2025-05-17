7ab886d995abc1a4adf49e5167ce74088b4ceca1
CrisN28 <Crislerobando10@gmail.com>
2025-05-16 23:33:59 -0600

    Added content to PowerShell script

--- 01-Azure-Virtual-Network/02-VNet-Peering/02-VNet-Peering-Setup.ps1
+++ 01-Azure-Virtual-Network/02-VNet-Peering/02-VNet-Peering-Setup.ps1
@@ -1,6 +1,5 @@
 # This script sets up virtual network peering between two virtual networks
 
-
 # Get the first virtual network named "Labs-Network"
 $vnet1 = Get-AzVirtualNetwork -Name "Labs-Network" -ResourceGroupName "RG-Labs-AZ104"
 
@@ -9,16 +8,17 @@ $vnet2 = Get-AzVirtualNetwork -Name "Labs-Network02" -ResourceGroupName "RG-Labs
 
 
 # Create peering from Labs-Network to Labs-Network02 (allow traffic)
+# The .Id property returns the unique Azure resource ID of each virtual network
 Add-AzVirtualNetworkPeering -Name "Labs-Network-to-Labs-Network02" `
   -VirtualNetwork $vnet1 `
-  -RemoteVirtualNetworkId $vnet2.Id `
-  -AllowVirtualNetworkAccess $true
+  -RemoteVirtualNetworkId $vnet2.Id
 
 # Create peering from Labs-Network02 to Labs-Network (allow traffic)
 Add-AzVirtualNetworkPeering -Name "Labs-Network02-to-Labs-Network" `
   -VirtualNetwork $vnet2 `
-  -RemoteVirtualNetworkId $vnet1.Id `
-  -AllowVirtualNetworkAccess $true
+  -RemoteVirtualNetworkId $vnet1.Id
 
 
-# The .Id property returns the unique Azure resource ID of each virtual network    
+# These commands verify that virtual network peering exists in both directions
+Get-AzVirtualNetworkPeering -ResourceGroupName "RG-Labs-AZ104" -VirtualNetworkName "Labs-Network" | Format-Table Name, PeeringState, RemoteVirtualNetworkId
+Get-AzVirtualNetworkPeering -ResourceGroupName "RG-Labs-AZ104" -VirtualNetworkName "Labs-Network02" | Format-Table Name, PeeringState, RemoteVirtualNetworkId
